1e0658513fab5d840f50a221ff99c159a74378bd
Taham-Jan <m.taham0101@gmail.com>
2023-03-18 20:36:42 +0500

    d

--- dist/app.js
+++ dist/app.js
@@ -37,7 +37,7 @@ app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.static(path_1.default.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path_1.default.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path_1.default.join(__dirname, "frontend", "build", "index.html"));
 });
 app.use('/api/todolist', ListRoutes_1.default);
 app.use((req, res, next) => {
--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
 });
  
 app.use('/api/todolist',listRoute);
