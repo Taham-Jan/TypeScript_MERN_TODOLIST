fd592204f32842398ddfba8e6f55018fd543a7f3
Taham-Jan <m.taham0101@gmail.com>
2023-03-18 18:52:27 +0500

    g

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,9 @@ import express from "express";
 import env from './util/validateEnv'
 import mongoose from "mongoose";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-import path from "path";
+
 const port = env.PORT;
-app.use(express.static(path.join(__dirname, './frontend/build')));
-app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-})
+
 mongoose.connect(env.MONGO_URI)
      .then(() => {
           console.log("MONGOOSE CONNECTED");
